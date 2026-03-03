cf20c3baffd7b53608521d8118d9bdb3ae706dba
Matthew <matthewjwright07@gmail.com>
2026-03-02 21:53:26 -0600

    initial commit

---
+++ public/script.js
@@ -0,0 +1,32 @@
+const addButton = document.querySelector("#addButton")
+const container = document.querySelector(".container")
+const input = document.querySelector("#input")
+
+addButton.addEventListener("click", ()=>{
+    createTodo()
+})
+
+document.addEventListener("keydown", (event)=>{
+    if (event.key === "Enter") {
+        createTodo()
+    }
+})
+
+function createTodo(){
+    let div = document.createElement("div")
+    div.classList.add("todo")
+
+    let image = document.createElement("img")
+    image.src = "./delete.svg"
+    image.addEventListener("click", ()=>{
+        div.remove()
+    })
+
+    let text = document.createElement("p")
+    text.innerText = input.value
+
+    div.append(image, text)
+
+    container.append(div)
+    input.value = ""
+}
